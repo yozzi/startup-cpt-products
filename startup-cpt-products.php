d93ea730ac9c7b8b087088e591979291902c99a6
yozz <yozz.online@gmail.com>
2015-09-04 14:19:47 -0400

    Version number

--- startup-cpt-products.php
+++ startup-cpt-products.php
@@ -3,7 +3,7 @@
 Plugin Name: StartUp Products
 Description: Le plugin pour activer le Custom Post Products
 Author: Yann Caplain
-Version: 0.1.0
+Version: 1.0.0
 Text Domain: startup-reloaded-products
 */
 
