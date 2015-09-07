97a6df442fab2b9e264032f0a86be8908b8f378d
yozz <yozz.online@gmail.com>
2015-09-07 09:58:22 -0400

    Flush permalinks on plugin activation

--- startup-cpt-products.php
+++ startup-cpt-products.php
@@ -84,6 +84,14 @@ function startup_reloaded_products() {
 
 add_action( 'init', 'startup_reloaded_products', 0 );
 
+//Flusher les permalink à l'activation du plgin pour qu'ils fonctionnent sans mise à jour manuelle
+function startup_reloaded_products_rewrite_flush() {
+    startup_reloaded_products();
+    flush_rewrite_rules();
+}
+
+register_activation_hook( __FILE__, 'startup_reloaded_products_rewrite_flush' );
+
 // Capabilities
 function startup_reloaded_products_caps() {
 	$role_admin = get_role( 'administrator' );
