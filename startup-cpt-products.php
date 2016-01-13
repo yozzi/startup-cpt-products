dce7a91f1771410aca741e4424fe245232cee8b4
yozz <yozz.online@gmail.com>
2016-01-13 16:39:13 -0500

    Format

--- startup-cpt-products.php
+++ startup-cpt-products.php
@@ -292,7 +292,7 @@ if ( !function_exists( 'shortcode_ui_detection' ) ) {
         }
     }
 
-add_action( 'init', 'shortcode_ui_detection' );
+    add_action( 'init', 'shortcode_ui_detection' );
 }
 
 function startup_cpt_products_shortcode_ui() {
